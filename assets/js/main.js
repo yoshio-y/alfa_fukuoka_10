f064a64329a6ac5b97040cf6fb91f2f2ec14524f
vvvvvvv <76511024+yoshio-y@users.noreply.github.com>
2021-09-12 19:05:36 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,10 +29,7 @@ const app = Vue.createApp({
           q1: 'ううう',
           // q2: 'かかか',
           // q3: 'ききき',
-        },
-        stage4: {
-          q1: 'えええ',
-        },
+        }
       },
 
       /* それぞれの問題が正解かどうか
@@ -51,9 +48,6 @@ const app = Vue.createApp({
           false, // 3-1
           // false, // 3-2
           // false, // 3-3
-        ],
-        stage4: [
-          false,
         ]
       },
 
@@ -62,7 +56,6 @@ const app = Vue.createApp({
         stage1: false,
         stage2: false,
         stage3: false,
-        stage4: false,
       },
 
       /* 次のステージを表示するかどうか
@@ -71,7 +64,6 @@ const app = Vue.createApp({
       next: {
         stage1: false,
         stage2: false,
-        stage3: false,
       },
     }
   },
